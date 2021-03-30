a242fc02413a6a4595b81cc075b2397f6c74ec63
benjaminearl <bnjmnearl@gmail.com>
2021-03-30 15:20:35 +0200

    add draw/erase button + make canvas stretch to height of page

--- drawing.js
+++ drawing.js
@@ -1,43 +1,64 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    const canvas = document.getElementById('canvas')
-    const ctx = canvas.getContext('2d')
-    let coord = { x: 0, y: 0 }
+    var drawButton = document.getElementById('drawBtn')
 
-    document.addEventListener('mousedown', start)
-    document.addEventListener('mouseup', stop)
-    window.addEventListener('resize', resize)
+    drawButton.addEventListener('mousedown', startDrawing)
 
-    function resize() {
-        ctx.canvas.width = window.innerWidth
-        ctx.canvas.height = window.innerHeight
+    function startDrawing() {
+
+        
+        const canvas = document.getElementById('canvas')
+
+        if(canvas.style.display === 'none'){
+            canvas.style.display = 'block'
+            drawButton.innerHTML = 'Erase'
+            drawButton.style.backgroundColor = 'red'
+            drawButton.style.color = 'white'
+        } else {
+            canvas.style.display = 'none'
+            drawButton.innerHTML = 'Draw'
+            drawButton.style.backgroundColor = 'yellow'
+            drawButton.style.color = 'black'
+        }
+
+        const ctx = canvas.getContext('2d')
+        let coord = { x: 0, y: 0 }
+
+        document.addEventListener('mousedown', start)
+        document.addEventListener('mouseup', stop)
+        window.addEventListener('resize', resize)
+
+        function resize() {
+            ctx.canvas.width = document.body.clientWidth
+            ctx.canvas.height = document.body.clientHeight
+        }
+
+        resize()
+
+        function start(event) {
+            document.addEventListener('mousemove', draw)
+            reposition(event)
+        }
+
+        function reposition(event) {
+            coord.x = event.clientX - canvas.offsetLeft
+            coord.y = event.clientY - canvas.offsetTop
+        }
+
+        function stop() {
+            document.removeEventListener('mousemove',draw)
+        }
+
+        function draw(event) {
+            ctx.beginPath()
+            ctx.lineWidth = 10
+            ctx.lineCap = 'round'
+            ctx.strokeStyle = 'blue'
+            ctx.moveTo(coord.x, coord.y)
+            reposition(event)
+            ctx.lineTo(coord.x, coord.y)
+            ctx.stroke()
+        }
+
     }
-
-    resize()
-
-    function start(event) {
-        document.addEventListener('mousemove', draw)
-        reposition(event)
-    }
-
-    function reposition(event) {
-        coord.x = event.clientX - canvas.offsetLeft
-        coord.y = event.clientY - canvas.offsetTop
-    }
-
-    function stop() {
-        document.removeEventListener('mousemove',draw)
-    }
-
-    function draw(event) {
-        ctx.beginPath()
-        ctx.lineWidth = 5
-        ctx.lineCap = 'round'
-        ctx.strokeStyle = 'blue'
-        ctx.moveTo(coord.x, coord.y)
-        reposition(event)
-        ctx.lineTo(coord.x, coord.y)
-        ctx.stroke()
-    }
-
 })
