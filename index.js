d7e8fbe564e93b0276e051b7c87d4e9a662903c9
benjaminearl <bnjmnearl@gmail.com>
2021-04-29 14:37:21 +0200

    add new box for info, change styling

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    var aboutButton =  document.getElementById("about-button")
-    var introLetter = document.getElementById("introduction")
+    var container = document.getElementById('info-container')
 
-    aboutButton.addEventListener('click', triggerLetter);
+    aboutContent = "<p>Group Therapy is a inter-master’s project support group that takes place at the Royal Academy of Art, The Hague. It is an attempt to form a space to experiment with learning methods and form connections between students from the different master’s courses at the academy. Ultimately it aims to be a format that can establish itself amongst the current courses and be passed down from one generation to the next.</p>"
 
-    function triggerLetter(){
-        if(introLetter.style.left === "200%"){
-            introLetter.style.left = "50%"
-        } else if (introLetter.style.left === "50%"){
-            introLetter.style.left = "200%"
+    exerciseContent = '<p>Exercise: 01<br>In this exercise we played with the physics of space and time to make a floobootooboo</p>'
+
+    var aboutBtn = document.getElementById('about-button')
+
+    aboutBtn.addEventListener("click", function(){
+        if(container.style.display === "none") {
+            container.style.display = "block"
+        } else {
+            container.style.display ="none"
         }
-    }
+        
+        container.innerHTML = aboutContent;
+    })
+    
 
 })
